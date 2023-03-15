284dcf804cc726a5050c2e5efc7e3058df46d346
oleksandr719 <olry719@gmail.com>
2023-03-15 16:53:24 +0200

    Update spec.cy.js

--- spec.cy.js
+++ spec.cy.js
@@ -54,4 +54,25 @@ describe('Regressionshoptests', () => {
    //cy.wait('.grand totals')
    cy.contains('Go to Checkout')//.click({force: true}) //check that product is added to Shopping cart         
 })  
+it('Verify if user can add product to compare page and trmove it ', () => {
+  cy.visit('https://regressionshop.dermpro.com/customer/account/login')
+  cy.get('#email')
+    .type('or.pixelmatic.test.2066@getnada.com').should('have.value', 'or.pixelmatic.test.2066@getnada.com') //fill login
+  cy.get('#pass')
+    .type('Ape4clam!').should('have.value', 'Ape4clam!') //fill password
+  cy.get('#send2')
+    .click({force: true})
+  cy.url().should('include','/customer/account') //check that login is successful
+  cy.get('a.level-top[title="Products"]') //go to Products category page
+    .click({force: true})    
+  //cy.wait('#cart-totals')
+  cy.contains('Add to Cart').first().click({force: true}) //open first product details page
+  cy.contains('Add to Wishlist').click({force: true}) //click on Add to cart button
+  //cy.wait('.grand totals')  
+})  
+
+
+
+
+
 })
